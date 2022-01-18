6255b6c3ff226e8b701c60e43d8436e73c9200e6
Raagul26 <57821148+Raagul26@users.noreply.github.com>
2022-01-18 18:23:08 +0530

    Initial commit

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Product, products } from '../products';
 import { CartService } from '../cart.service';
 import {MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition} from '@angular/material/snack-bar';
+import { DomSanitizer } from '@angular/platform-browser';
 
 
 @Component({
@@ -13,7 +14,16 @@ import {MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition}
 export class ProductDetailsComponent implements OnInit {
 
   product:Product | any
-  constructor(private route: ActivatedRoute, private cartService:CartService, private snackBar: MatSnackBar, private router:Router) { }
+
+  star:string = `<svg xmlns="http://www.w3.org/2000/svg" style="height: 25px; width:25px;fill:#FDCC0D" viewBox="0 0 24 24">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
+  </svg>`
+  
+  rating = document.getElementById("rating-container")
+
+  starRating:any=""
+
+  constructor(private route: ActivatedRoute, private cartService:CartService, private snackBar: MatSnackBar, private router:Router, private sanitizer:DomSanitizer) { }
 
   horizontalPosition: MatSnackBarHorizontalPosition = 'center';
   verticalPosition: MatSnackBarVerticalPosition = 'top';
@@ -22,6 +32,7 @@ export class ProductDetailsComponent implements OnInit {
     const routeParams = this.route.snapshot.paramMap
     const productIdFromRoute = Number(routeParams.get('productId'))
     this.product = products.find(product => product.id === productIdFromRoute)
+    this.starRating=this.sanitizer.bypassSecurityTrustHtml(this.star.repeat(this.product.rating.rate))
   }
 
   addToCart(product:Product)
